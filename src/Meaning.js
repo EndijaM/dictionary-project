0d731ae00de119b2ac21cad2186f2ca539f2c731
EndijaM <115448772+EndijaM@users.noreply.github.com>
2023-01-07 20:46:42 +0100

    Rendered the searched word meanings and definitions

---
+++ src/Meaning.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Meaning(props) {
+  console.log(props.meaning);
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+
+      {props.meaning.definitions.map(function (definitions, index) {
+        return (
+          <div key={index}>
+            <p>
+              {definitions.definition}
+              <br />
+
+              <em>{definitions.example}</em>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
