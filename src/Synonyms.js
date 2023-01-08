3d9c7260673045181e83eb228679c5f0dd77009d
EndijaM <115448772+EndijaM@users.noreply.github.com>
2023-01-08 14:16:10 +0100

    Updating if word length statement for Synonyms component

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 export default function Synonyms(props) {
-  if (props.synonyms) {
+  if (props.synonyms.length > 0) {
     return (
       <ul className="Synonyms">
-        {" "}
+        <strong>Synonyms:</strong>{" "}
         {props.synonyms.map(function (synonym, index) {
           return <li key={index}>{synonym}</li>;
         })}
