3708c41e9fa42a31fa66361286271b19a66a51c6
EndijaM <115448772+EndijaM@users.noreply.github.com>
2023-01-15 16:11:29 +0100

    Added a react audio player

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "./Phonetics.css";
+import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetics(props) {
   //console.log(props.phonetic);
   return (
     <div className="Phonetics">
-      <span className="pronounciation">{props.phonetic.text}</span>{" "}
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        Listen
-      </a>
+      <div className="text-center text-sm-start row flex-wrap-reverse">
+        {" "}
+        <span className="pronounciation col">{props.phonetic.text}</span>{" "}
+        <ReactAudioPlayer
+          src={props.phonetic.audio}
+          controls
+          className="audio-player col-sm-8"
+        />
+      </div>
     </div>
   );
 }
